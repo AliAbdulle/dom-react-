2a2823f0c84f3dedcedab67b595df18d1504eab8
AliAbdulle <alidhobey@gmail.com>
2020-05-27 15:54:55 -0500

    export from the other page and create the tap

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import {Link, Switch, Route} from 'react-router-dom'
+import {Link, Switch,Router} from 'react-router-dom'
 
 import Info from './Info'
 import Setting  from'./Setting'
 
 function Profile() {
-    return {
-
-    }
+    return(
+        <div>
+            <h1>Profile Page</h1>
+            <ul>
+                <li><Link to="/profile/info">Profile Info</Link></li>
+                <li><Link to="/profile/setting">Profile Setting</Link></li>
+            </ul>
+            
+            <Switch>
+                <Router path="/profile/info"></Router>
+                <Router path="/profile/setting"></Router>
+            </Switch>
+        </div>
+    )
 }
 export default Profile
