d1ebc5a292e709bae6092795737062dcb34af242
AliAbdulle <alidhobey@gmail.com>
2020-05-26 21:43:43 -0500

    Saved function App error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
+ 
+//import {Switch, Route} from 'raect-router-dom'
 
 function App() {
   return (
